81b5557bdec6c0d7fd2ac9e097caf373d608bf7f
Pavel Loginov <lgnv2009@gmail.com>
2019-04-07 22:57:09 +0300

    Add vector extension methods

--- src/Extensions/VectorExtensions.cs
+++ src/Extensions/VectorExtensions.cs
@@ -13,5 +13,17 @@ namespace Extras.Extensions
 
         // Returns a cross product of two vectors
         public static Vector3 Cross(this Vector3 v3, Vector3 other) => Vector3.Cross(v3, other);
+
+        // Returns a minimal component of the vector
+        public static float Min(this Vector2 v2) => Mathf.Min(v2.x, v2.y);
+        public static float Min(this Vector3 v3) => Mathf.Min(v3.x, Mathf.Min(v3.y, v3.z));
+
+        // Returns a maximal component of vector
+        public static float Max(this Vector2 v2) => Mathf.Max(v2.x, v2.y);
+        public static float Max(this Vector3 v3) => Mathf.Max(v3.x, Mathf.Max(v3.y, v3.z));
+
+        // Checks that vector length is close to zero
+        public static bool IsZero(this Vector2 v2) => v2 == Vector2.zero || Vector2.SqrMagnitude(v2) < float.Epsilon;
+        public static bool IsZero(this Vector3 v3) => v3 == Vector3.zero || Vector3.SqrMagnitude(v3) < float.Epsilon;
     }
 }
