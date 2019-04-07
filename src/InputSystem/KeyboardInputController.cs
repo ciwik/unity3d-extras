e41c6194d5c0d150f2cf0040dbd8119fda139171
Pavel Loginov <lgnv2009@gmail.com>
2019-04-07 21:49:35 +0300

    Add an example implementation of Input controller

---
+++ src/InputSystem/KeyboardInputController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Extras.InputSystem
+{
+// TODO: It is just a silly example, should be transferred to the Ministry of silly examples
+    public class KeyboardInputController : BaseInputController
+    {
+        private void Update()
+        {
+            if (Input.anyKey)
+            {
+                var axisValue = Input.GetAxis("Horizontal");
+                if (axisValue != 0f)
+                {
+                    var direction = axisValue * Vector2.right;
+                    _bindings.Movement?.Invoke(direction);
+                }
+
+                axisValue = Input.GetAxis("Jump");
+                if (axisValue != 0f)
+                {
+                    _bindings.Action?.Invoke();
+                }
+            }
+        }
+    }
+}
