4bfa97937aff0c6773cef59bc37c58691a532c66
Pavel Loginov <lgnv2009@gmail.com>
2019-04-07 21:09:51 +0300

    Refactoring of Camera.TakeSnapshot extension method

--- src/Extensions/CameraExtensions.cs
+++ src/Extensions/CameraExtensions.cs
@@ -7,16 +7,21 @@ namespace Extras.Extensions
         // Returns a snapshot of the camera view
         public static Texture2D TakeSnapshot(
             this Camera camera,
-            int width = DEFAULT_TEXTURE_WIDTH,
-            int height = DEFAULT_TEXTURE_HEIGHT)
+            int? width = null,
+            int? height = null,
+            TextureFormat format = DEFAULT_TEXTURE_FORMAT,
+            int colorDepth = DEFAULT_COLOR_DEPTH)
         {
-            var renderTexture = new RenderTexture(width, height, COLOR_DEPTH);
-            var texture = new Texture2D(width, height, TextureFormat.RGB24, false);
+            var pixelWidth = width ?? camera.pixelWidth;
+            var pixelHeight = height ?? camera.pixelHeight;
+
+            var renderTexture = new RenderTexture(pixelWidth, pixelHeight, colorDepth);
+            var texture = new Texture2D(pixelWidth, pixelHeight, format, false);
 
             camera.targetTexture = renderTexture;
             camera.Render();
             RenderTexture.active = renderTexture;
-            texture.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+            texture.ReadPixels(new Rect(0, 0, pixelWidth, pixelHeight), 0, 0);
 
             RenderTexture.active = null;
             camera.targetTexture = null;
@@ -25,10 +30,8 @@ namespace Extras.Extensions
             return texture;
         }
 
-        // TODO: Remove
         // Default values
-        private const int COLOR_DEPTH            = 24;
-        private const int DEFAULT_TEXTURE_WIDTH  = 1280;
-        private const int DEFAULT_TEXTURE_HEIGHT = 720;
+        private const int DEFAULT_COLOR_DEPTH = 24;
+        private const TextureFormat DEFAULT_TEXTURE_FORMAT = TextureFormat.RGB24;
     }
 }
