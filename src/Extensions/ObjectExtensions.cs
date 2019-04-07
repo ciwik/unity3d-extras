714fb20806a2e2589615b3bcc23a6132fe664450
Pavel Loginov <lgnv2009@gmail.com>
2019-04-07 20:07:53 +0300

    Add ObjectExtensions

---
+++ src/Extensions/ObjectExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Extras.Extensions
+{
+    public static class ObjectExtensions
+    {
+        // Apply some actions for object and return it
+        public static T Tap<T>(this T obj, Action<T> action)
+        {
+            action(obj);
+            return obj;
+        }
+    }
+}
