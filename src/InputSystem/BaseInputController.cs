a41bde8f4acca658ae607361b9b626426679e47c
Pavel Loginov <lgnv2009@gmail.com>
2019-04-12 11:10:08 +0300

    Delete example of input system implementation

--- src/InputSystem/BaseInputController.cs
+++ src/InputSystem/BaseInputController.cs
@@ -9,3 +9,27 @@ namespace Extras.InputSystem
         protected InputBindings _bindings;
     }
 }
+
+// Example
+//
+//public class KeyboardInputController : BaseInputController
+//{
+//private void Update()
+//{
+//    if (Input.anyKey)
+//    {
+//        var axisValue = Input.GetAxis("Horizontal");
+//        if (axisValue != 0f)
+//        {
+//            var direction = axisValue * Vector2.right;
+//            _bindings.Movement?.Invoke(direction);
+//        }
+//
+//        axisValue = Input.GetAxis("Jump");
+//        if (axisValue != 0f)
+//        {
+//            _bindings.Action?.Invoke();
+//        }
+//    }
+//}
+//}
