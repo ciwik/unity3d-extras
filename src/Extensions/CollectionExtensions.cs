e085e16517713e94c93cadfd84a3447be5ca695f
Pavel Loginov <lgnv2009@gmail.com>
2019-06-27 11:15:46 +0300

    Add IList.Shuffle extension method

--- src/Extensions/CollectionExtensions.cs
+++ src/Extensions/CollectionExtensions.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using UnityEngine;
 
 namespace Extras.Extensions
 {
@@ -18,5 +19,23 @@ namespace Extras.Extensions
 
         // Returns a read-only copy of list
         public static IReadOnlyCollection<T> AsReadOnly<T>(this IList<T> list) => new ReadOnlyCollection<T>(list);
+
+        // Returns the same list with shuffled items
+        public static IList<T> Shuffle<T>(this IList<T> list)
+        {
+            var n = list.Count;
+
+            while (n > 1)
+            {
+                var k = Random.Range(0, n);
+                n--;
+
+                var value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+
+            return list;
+        }
     }
 }
